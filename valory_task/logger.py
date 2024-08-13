33a8788ba0201cd446de30392b78e54c5cb45398
actionless <actionless.loveless@gmail.com>
2024-08-13 19:14:47 +0200

    feat(logger): add colors and formatting

--- valory_task/logger.py
+++ valory_task/logger.py
@@ -2,7 +2,58 @@ from __future__ import annotations
 
 import concurrent.futures
 from logging import DEBUG, ERROR, INFO, WARNING
-from typing import Any
+from typing import Any, Final
+
+BOLD_START: Final = "\033[0;1m"
+BOLD_RESET: Final = "\033[0m"
+COLOR_RESET: Final = "\033[0;0m"
+
+
+class Colors:
+    black = 0
+    red = 1
+    green = 2
+    yellow = 3
+    blue = 4
+    purple = 5
+    cyan = 6
+    white = 7
+
+
+class ColorsHighlight:
+    black = 8
+    red = 9
+    green = 10
+    yellow = 11
+    blue = 12
+    purple = 13
+    cyan = 14
+    white = 15
+
+
+def color_start(
+        color_number: int,
+) -> str:
+    result = ""
+    if color_number >= ColorsHighlight.black:
+        result += "\033[0;1m"
+        color_number -= ColorsHighlight.black
+    result += f"\033[03{color_number}m"
+    return result
+
+
+def color_line(
+        line: str, color_number: int, *, reset: bool = True,
+) -> str:
+    result = f"{color_start(color_number)}{line}"
+    # reset font:
+    if reset:
+        result += COLOR_RESET
+    return result
+
+
+def bold_line(line: str) -> str:
+    return f"{BOLD_START}{line}{BOLD_RESET}"
 
 
 class AsyncPrint:
@@ -28,27 +79,27 @@ class AsyncLogging:
 
     @staticmethod
     def log(msg: str, *args: Any) -> None:
-        AsyncPrint.print(msg % args)
+        AsyncPrint.print(msg % tuple(bold_line(arg) for arg in args))
 
     @classmethod
     def debug(cls, msg: str, *args: Any) -> None:
         if cls.level <= DEBUG:
-            cls.log(f"DEBUG {msg}", *args)
+            cls.log(f"{color_line('DEBUG', ColorsHighlight.black)} {msg}", *args)
 
     @classmethod
     def info(cls, msg: str, *args: Any) -> None:
         if cls.level <= INFO:
-            cls.log(f"INFO  {msg}", *args)
+            cls.log(f"{color_line('INFO', ColorsHighlight.cyan)}  {msg}", *args)
 
     @classmethod
     def warning(cls, msg: str, *args: Any) -> None:
         if cls.level <= WARNING:
-            cls.log(f"WARN  {msg}", *args)
+            cls.log(f"{color_line('WARN', ColorsHighlight.yellow)}  {msg}", *args)
 
     @classmethod
     def error(cls, msg: str, *args: Any) -> None:
         if cls.level <= ERROR:
-            cls.log(f"ERROR {msg}", *args)
+            cls.log(f"{color_line('ERROR', ColorsHighlight.red)} {msg}", *args)
 
 
 def debug(msg: str, *args: Any) -> None:
